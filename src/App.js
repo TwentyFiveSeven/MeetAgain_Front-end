2747deea10704c019c3e10e81b7c8d06d471887f
psh <keep4404@gamil.com>
2020-08-07 17:09:43 +0900

    merge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import Header from "./components/Header.js"
-import Body from "./components/Body.js"
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Header.js";
+import Body from "./components/Body.js";
+import * as Pages from "./pages";
+import "./App.css";
 
-class App extends Component {
-  render(){
-    return (
-      <div>
-        <Header>
-
-        </Header>
-        <Body>
-        
-        </Body>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exac path="/signup" component={Pages.LoginPage} />
+        <Route exac path="/main" component={Pages.LoginPage} />
+        <Route exac path="/post" component={Pages.PostListPage} />
+        <Route exac path="/" component={Pages.LoginPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
+// class App extends Component {
+//   render(){
+//     return (
+//       <div>
+//         <Header>
+
+//         </Header>
+//         <Body>
+
+//         </Body>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
